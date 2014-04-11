1048ecfdb8b3fcc90b9c26df5d4ecbf40fcb00b3
Yuki Takei <yuki@weseek.co.jp>
2014-04-11 16:55:15 +0900

    modify description

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,6 @@ name             'chef-ec2util'
 maintainer       'WESEEK, Inc.'
 maintainer_email 'syunsuke@weseek.co.jp'
 license          'All rights reserved'
-description      'Utility cookbook'
+description      'Utility cookbook for Amazon EC2'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
