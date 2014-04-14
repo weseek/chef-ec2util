fd55088d089c3c6a9c362d46f2a313c33bb07b92
Yuki Takei <yuki@weseek.co.jp>
2014-04-14 12:01:27 +0900

    add 'self.' (define as class method)

--- libraries/util.rb
+++ libraries/util.rb
@@ -2,15 +2,15 @@ require 'json'
 
 class Chef::Recipe::EC2Util
   
-  def get_instance_id
+  def self.get_instance_id
     (`curl -s http://169.254.169.254/latest/meta-data/instance-id`).strip rescue ""
   end
   
-  def get_instance_az
+  def self.get_instance_az
     (`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`).strip rescue ""
   end
   
-  def get_instance_region
+  def self.get_instance_region
     JSON.parse((`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document`).strip)["region"] rescue ""
   end
   
